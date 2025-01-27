8bdde2f26081dd731162f08c17897efbaa2aced6
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2025-01-27 21:50:30 +0530

    Create Wave Array.cpp

---
+++ Arrays/Wave Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // arr: input array
+    // Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr) {
+        int n = arr.size();
+        
+        // Swap adjacent elements
+        for (int i = 0; i < n - 1; i += 2) {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+};
