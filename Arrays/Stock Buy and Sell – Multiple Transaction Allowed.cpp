6f38f1963fa283d3fecb04138b995041243b9994
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2025-02-01 20:12:24 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed.cpp

---
+++ Arrays/Stock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -0,0 +1,14 @@
+//Stock Buy and Sell – Multiple Transaction Allowed
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i =1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                profit+= priices[i] - prices[i-1];
+            }
+        }
+        
+    }
+};
