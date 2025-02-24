06ee134fbd1e22b9df38d951ea600c381186b5b6
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2025-02-24 21:40:16 +0530

    Update Unique Permutations.cpp

--- Recursion/Unique Permutations.cpp
+++ Recursion/Unique Permutations.cpp
@@ -2,22 +2,26 @@
 
 class Solution {
   public:
+// backtrack() helps to generate permutations
     void backtrack(vector<int>& arr, int start, set<vector<int>>& uniquePermutations) {
-        if (start == arr.size()) {
-            uniquePermutations.insert(arr);
+        if (start == arr.size()) { //the recursion would stop when the start reaches the length of the string or array
+            uniquePermutations.insert(arr); // the current permutation of arr will be inserted in the vector
             return;
         }
         for (int i = start; i < arr.size(); i++) {
-            swap(arr[start], arr[i]);
-            backtrack(arr, start + 1, uniquePermutations);
-            swap(arr[start], arr[i]); // Backtrack
+            swap(arr[start], arr[i]);//Swap the numbers or characters to explore different position
+            backtrack(arr, start + 1, uniquePermutations);// moves to the next index to generate further permutations
+            swap(arr[start], arr[i]); // Backtracks - it restores the original order of the string so it would not affect the future recursive calls
         }
     }
-    
+
+//uniquePerms() helps to return the final result that is the unique permutations
     vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
         sort(arr.begin(), arr.end()); // Sort to get lexicographical order
         set<vector<int>> uniquePermutations;
-        backtrack(arr, 0, uniquePermutations);
-        return vector<vector<int>>(uniquePermutations.begin(), uniquePermutations.end());
+        backtrack(arr, 0, uniquePermutations); // starts the recursive permutation generation
+        return vector<vector<int>>(uniquePermutations.begin(), uniquePermutations.end()); //set to vector conversion
     }
 };
+
+
