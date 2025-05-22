4c15f87933585bf45de813a47cff9c0a19eecf79
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2025-05-22 10:23:41 +0530

    Update Count Digits.cpp

--- Math/Count Digits.cpp
+++ Math/Count Digits.cpp
@@ -1,17 +1,15 @@
 class Solution {
-  public:
-    int evenlyDivides(int n) {
+public:
+    int countDigit(int n) {
+        if(n==0) return 1;
         int count = 0;
-        int original = n; // Store the original number
-        
-        while (n != 0) {
-            int d = n % 10; // Extract the last digit
-            n = n / 10;     // Remove the last digit
+        while(n!=0){
+            int digit = n%10;
+            count++;
+            n = n/10;
 
-            if (d != 0 && original % d == 0) { // Ignore 0 and check divisibility
-                count++;
-            }
         }
         return count;
+
     }
 };
