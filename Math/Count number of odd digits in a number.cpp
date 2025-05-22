355fd203c60d396f31dc36c8441370aad33875bc
Akanksha Panda <127029009+akpanda01@users.noreply.github.com>
2025-05-22 10:27:25 +0530

    Create Count number of odd digits in a number.cpp

---
+++ Math/Count number of odd digits in a number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOddDigit(int n) {
+        int count = 0;
+
+        while(n!=0){
+            int digit = n%10;
+            if(digit % 2 !=0){
+                count++;
+            }
+            n=n/10;
+        }
+        return count;
+    }
+};
